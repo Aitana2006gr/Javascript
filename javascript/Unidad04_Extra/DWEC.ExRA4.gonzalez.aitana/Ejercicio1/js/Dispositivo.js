85616f270c2b3af7df072af3ceaa75e13669854d
agonzalezr34 <agonzalezr34@educantabria.es>
2025-12-12 10:06:38 +0100

    Terminado ejercicio1

--- javascript/Unidad04_Extra/DWEC.ExRA4.gonzalez.aitana/Ejercicio1/js/Dispositivo.js
+++ javascript/Unidad04_Extra/DWEC.ExRA4.gonzalez.aitana/Ejercicio1/js/Dispositivo.js
@@ -1,56 +1,63 @@
 //Clase Dispositivo definida mediante prototipos
-function Dispositivo() {
+function Dispositivo(_id="NOID",_autonomia=0,_carga=0) { //Constructor
     //Valores por defecto
-    this._id = "NOID";
-    this._autonomia = 0;
-    this._carga = 0;
+    this._id = _id;
+    this._autonomia = _autonomia;
+    this._carga = _carga;
 }
 
 //Setters
 Dispositivo.prototype.setID = function(id) {
-    if (!id) return;
-
+    //Convierte la entrada id a mayúsculas con el toUppercase
     let mayusculas = id.toUpperCase();
+    //Comprueba con el método verificarID y si es true, modificará el valor de la propiedad
     if (this.verificarID(mayusculas)) {
         this._id = mayusculas;
     }
 };
 
 Dispositivo.prototype.setAutonomia = function(valor) {
+    //Asigna el valor recibido a _autonomia
     this._autonomia = valor;
 };
 
 Dispositivo.prototype.setCarga = function(valor) {
+    //Comprueba que el valor sea entero entre 0 y 100 (incluidos)
     if (Number.isInteger(valor) && valor >= 0 && valor <= 100) {
+        //Se asigna el valor a _carga
         this._carga = valor;
     }
 };
 
 //Getters
 Dispositivo.prototype.getID = function() {
+    //Devuelve el valor de _id
     return this._id;
 };
 
 Dispositivo.prototype.getAutonomia = function() {
+    //Devuelve el valor de _autonomia
     return this._autonomia;
 };
 
 Dispositivo.prototype.getCarga = function() {
+    //Devuelve el valor de _carga
     return this._carga;
 };
 
 //Métodos
 //Método toString para mostrar la información
 Dispositivo.prototype.toString = function() {
+    //Devuelve la siguiente cadena de texto con sus pripiedades
     return `DISP: ${this._id}; ${this._autonomia}; ${this._carga};`;
 };
 
-//Método verificarID que devuelve booleano y comprueba si la cadena es mayor o igual a 10
+//Método verificarID que devuelve booleano y devuelve true o false si la cadena es mayor o igual a 10
 Dispositivo.prototype.verificarID = function(id) {
     return id.length >= 10;
 };
 
-//método horasRestantes que devuelve las horas restantes
+//método horasRestantes que devuelve true o false las horas restantes
 Dispositivo.prototype.horasRestantes = function(uso) {
     return uso * this._autonomia * this._carga / 100;
 };
--- javascript/Unidad04_Extra/DWEC.ExRA4.gonzalez.aitana/Ejercicio1/js/Telefono.js
+++ javascript/Unidad04_Extra/DWEC.ExRA4.gonzalez.aitana/Ejercicio1/js/Telefono.js
@@ -1,54 +1,74 @@
+//Constante para el array de tipos de panel 
+const tiposPanelArr = ["OLED", "AMOLED", "QLED", "NanoCell"];
+
 //Clase Telefono, hereda de Dispositivo (Dispositivo es su clase padre)
-function Telefono() {
-    Dispositivo.call(this); //Llama al constructor padre
+function Telefono(_id, _autonomia, _carga, _pulgadas = 0, _tipoPanel = 0) {
+    Dispositivo.call(this, _id, _autonomia, _carga); //Llama al constructor padre
 
     //Valores por defecto
-    this._pulgadas = 0;
-    this._tipoPanel = 0;
-    this._tiposPanelArr = ["OLED", "AMOLED", "QLED", "NanoCell"];
+    this._pulgadas = _pulgadas;
+    this._tipoPanel = _tipoPanel;
+    //this._tiposPanelArr = ["OLED", "AMOLED", "QLED", "NanoCell"]; No puedo definirla dentro del constructor, ya que de esta forma, estoy creando una copia pra cada instancia de Telefono.
+    //El contenido es el mismo pero es susceptible a ser modificado en esa instancia. Por ello, para que la propiedad _tiposPanelArr no se pueda modificar, la defino como una constante del prototipo fuera del constructor 
 }
 
 //Herencia del prototipo Dispositivo
 Telefono.prototype = Object.create(Dispositivo.prototype);
 Telefono.prototype.constructor = Telefono;
+//Añado la propiedad _tiposPanelArr
+Telefono.prototype._tiposPanelArr = tiposPanelArr;
 
 //Setters
-Telefono.prototype.setPulgaddas = function(valor) {
+Telefono.prototype.setPulgadas = function (valor) {
+    //Asigna el valor a _pulgadas
     this._pulgadas = valor;
 };
 
-Telefono.prototype.setTipoPanel = function(tipo) {
+Telefono.prototype.setTipoPanel = function (tipo) {
+    //Comprueba si el valor es menor que 0 o mayor que 3
     if (tipo < 0 || tipo > 3) {
-        this._tipoPanel = 0;
+        this._tipoPanel = 0; //Si no es así, se le asigna el valor 0
     } else {
-        this._tipoPanel = tipo;
+        this._tipoPanel = tipo; //Sino se le asigna el nuevo valor
     }
 };
 
 //Getters
-Telefono.prototype.getPulgadas = function() {
+Telefono.prototype.getPulgadas = function () {
+    //Devuelve el valor pulgadas
     return this._pulgadas;
 };
 
-Telefono.prototype.getTipoPanel = function() {
+Telefono.prototype.getTipoPanel = function () {
+    //Devuelve el valor de la propiedad _tipoPanel
     return this._tipoPanel;
 };
 
-Telefono.prototype.getTipoPanelTexto = function() {
+Telefono.prototype.getTipoPanelTexto = function () {
+    //Devuelve el tipo de panel en formato texto, utiliza la propiedad _tipoPanel
+    //como índice en el array _tiposPanelArr
     return this._tiposPanelArr[this._tipoPanel];
 };
 
 //Métodos
-//Método toString para mostrar la información del telefono
-Telefono.prototype.toString = function() {
+//Método toString que sobreescribe el método de la clase Dispositivo
+Telefono.prototype.toString = function () {
     return (
-        Dispositivo.prototype.toString.call(this) +
-        `${this._pulgadas}; ${this._tipoPanel};`
+        Dispositivo.prototype.toString.call(this) + //LLamo al metodo toString de la clase Dispositivo 
+        `${this._pulgadas}; ${this._tipoPanel};` //Añado las otras propiedades a la cadena de texto
     );
 };
 
-//Método verificarID que devuelve booleano y comprueba si la cadena comienza con "TELF -" comprobando que sea mayor o igual a 10 y menor o igual a 20
-Telefono.prototype.verificarID = function(id) {
-    if (!id.startsWith("TELF - ")) return false;
-    return id.length >= 10 && id.length <= 20;
+//Método verificarID que que sobreescribe el método de la clase Dispositivo
+Telefono.prototype.verificarID = function (id) {
+    //Para comprobar si empieza con "TELF "
+    if (!id.startsWith("TELF ")) {
+        return false;
+    }
+    //para comprobar que debe tener el menos 10 chars y un máximo de 20 en total 
+    if (id.length < 10 || id.length > 20) {
+        return false;
+    }
+    //Si cumple ambas, devuelve true.
+    return true;
 };
